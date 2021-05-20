a54da74ba1cf26065dbd6a1045d035cbf8af6676
Pedro Alves <pta2002@pta2002.com>
2021-05-20 20:10:13 +0100

    Basic support for noexpandtab
    Has some issues, particularly when tabstop != shiftwidth

--- lua/intellitab/init.lua
+++ lua/intellitab/init.lua
@@ -1,5 +1,19 @@
 local v = vim.api
 
+local function get_line_indent(line, sw)
+  local indent = 0;
+
+  for c in line:gmatch("%s") do
+    if c == "\t" then
+      indent = indent + sw
+    else
+      indent = indent + 1
+    end
+  end
+
+  return indent
+end
+
 local function indent()
   local cursor = v.nvim_win_get_cursor(0)
   local line = v.nvim_buf_get_lines(0, cursor[1] - 1, cursor[1], false)[1]
@@ -7,6 +21,8 @@ local function indent()
   -- TODO: support noexpand
   local indentexpr = v.nvim_buf_get_option(0, "indentexpr")
   local expand = v.nvim_buf_get_option(0, "expandtab")
+  local shiftwidth = v.nvim_eval("shiftwidth()")
+  local tab_char = v.nvim_replace_termcodes("<Tab>", true, true, true)
   local indent_goal
 
   -- TODO: Support treesitter indent
@@ -18,19 +34,29 @@ local function indent()
     indent_goal = v.nvim_eval("indent(v:lnum)")
   end
 
+  if indent_goal == -1 and cursor[1] ~= 1 then
+    local prev_line = v.nvim_buf_get_lines(0, cursor[1] - 2, cursor[1] - 1, false)[1]
+    indent_goal = get_line_indent(prev_line, shiftwidth)
+  end
+
   -- Reset the cursor, since the functions are free to move it
   v.nvim_win_set_cursor(0, cursor)
 
   -- TODO: Support indent_goal = -1, which means that we keep the previous
   -- indentation
-  if cursor[2] == 0 and line == "" and indent_goal > 0 and expand then
+  if cursor[2] == 0 and line == "" and indent_goal > 0 then
     local i = 0
     while i < indent_goal do
-      v.nvim_feedkeys(" ", 'n', true)
-      i = i + 1
+      if expand then
+        v.nvim_feedkeys(" ", 'n', true)
+        i = i + 1
+      else
+        v.nvim_feedkeys(tab_char, 'n', true)
+        i = i + shiftwidth
+      end
     end
+    print(i, indent_goal)
   else
-    local tab_char = v.nvim_replace_termcodes("<Tab>", true, true, true)
     v.nvim_feedkeys(tab_char, 'n', true)
   end
 end
